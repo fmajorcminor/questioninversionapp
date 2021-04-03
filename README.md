35531b3ed6efcdc550473f2ba4fcb7011161376f
Dallin Christensen <dallinc2@gmail.com>
2021-04-02 20:59:35 -0600

    Account for commas, proper nouns, and some others

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Application for inverting French questions
 
-### **Notes**
-- If word is captilized vs. non-capitilized, it changes confidence level. I've only seen this with Comment. More to come
+
+
+######Interesting things:
+- If word is capitalized vs. non-capitalized, it changes confidence level. I've only seen this with Comment. More to come
+
+######Current Problems:
+- D\'ou doesn't work - it splits it into two words
+
+######Frustrating things:
+- Figuring out commas were a nightmare
+- Was going to try to figure out capitalization, but that was also a nightmare
--- detectSyntax/detectSyntaxTest.py
+++ detectSyntax/detectSyntaxTest.py
@@ -9,11 +9,53 @@ Perform syntax analysis on French phrases to convert them to inverted questions
 """
 
 import logging
-from pprint import pprint
 import boto3
 from botocore.exceptions import ClientError
 
 logger = logging.getLogger(__name__)
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+class ComprehendDetect:
+    """Encapsulates Comprehend detection functions."""
+
+    def __init__(self, comprehend_client):
+        """
+        :param comprehend_client: A Boto3 Comprehend client.
+        """
+        self.comprehend_client = comprehend_client
+
+    def detect_syntax(self, text, language_code):
+        """
+        Detects syntactical elements of a document. Syntax tokens are portions of
+        text along with their use as parts of speech, such as nouns, verbs, and
+        interjections.
+
+        :param text: The document to inspect.
+        :param language_code: The language of the document.
+        :return: The list of syntax tokens along with their confidence scores.
+        """
+        try:
+            response = self.comprehend_client.detect_syntax(
+                Text=text, LanguageCode=language_code)
+            tokens = response['SyntaxTokens']
+            # logger.info("Detected %s syntax tokens.", len(tokens))
+        except ClientError:
+            logger.exception("Couldn't detect syntax.")
+            raise
+        else:
+            return tokens
+
 
 partsOfSpeech = ['ADJ', 'ADP', 'ADV', 'AUX', 'CCONJ', 'DET',
                  'INTJ', 'NOUN', 'NUM', 'O', 'PART', 'PRON',
@@ -31,20 +73,20 @@ TestPhrasePunct = [{'TokenId': 1, 'Text': 'Tu', 'BeginOffset': 0, 'EndOffset': 2
                     'PartOfSpeech': {'Tag': 'NOUN', 'Score': 0.9990205764770508}},
                    {'TokenId': 6, 'Text': '?', 'BeginOffset': 24, 'EndOffset': 25,
                     'PartOfSpeech': {'Tag': 'PUNCT', 'Score': 0.9999500513076782}}]
-                   # {'TokenId': 7, 'Text': 'Moi', 'BeginOffset': 26, 'EndOffset': 29,
-                   #  'PartOfSpeech': {'Tag': 'PRON', 'Score': 0.9687632322311401}},
-                   # {'TokenId': 8, 'Text': ',', 'BeginOffset': 29, 'EndOffset': 30,
-                   #  'PartOfSpeech': {'Tag': 'PUNCT', 'Score': 0.9999955892562866}},
-                   # {'TokenId': 9, 'Text': 'je', 'BeginOffset': 31, 'EndOffset': 33,
-                   #  'PartOfSpeech': {'Tag': 'PRON', 'Score': 0.9999996423721313}},
-                   # {'TokenId': 10, 'Text': 'veux', 'BeginOffset': 34, 'EndOffset': 38,
-                   #  'PartOfSpeech': {'Tag': 'VERB', 'Score': 0.7699386477470398}},
-                   # {'TokenId': 11, 'Text': 'te', 'BeginOffset': 39, 'EndOffset': 41,
-                   #  'PartOfSpeech': {'Tag': 'PRON', 'Score': 0.9978506565093994}},
-                   # {'TokenId': 12, 'Text': 'parler', 'BeginOffset': 42, 'EndOffset': 48,
-                   #  'PartOfSpeech': {'Tag': 'VERB', 'Score': 0.9999996423721313}},
-                   # {'TokenId': 13, 'Text': '.', 'BeginOffset': 48, 'EndOffset': 49,
-                   #  'PartOfSpeech': {'Tag': 'PUNCT', 'Score': 0.999948263168335}}]
+# {'TokenId': 7, 'Text': 'Moi', 'BeginOffset': 26, 'EndOffset': 29,
+#  'PartOfSpeech': {'Tag': 'PRON', 'Score': 0.9687632322311401}},
+# {'TokenId': 8, 'Text': ',', 'BeginOffset': 29, 'EndOffset': 30,
+#  'PartOfSpeech': {'Tag': 'PUNCT', 'Score': 0.9999955892562866}},
+# {'TokenId': 9, 'Text': 'je', 'BeginOffset': 31, 'EndOffset': 33,
+#  'PartOfSpeech': {'Tag': 'PRON', 'Score': 0.9999996423721313}},
+# {'TokenId': 10, 'Text': 'veux', 'BeginOffset': 34, 'EndOffset': 38,
+#  'PartOfSpeech': {'Tag': 'VERB', 'Score': 0.7699386477470398}},
+# {'TokenId': 11, 'Text': 'te', 'BeginOffset': 39, 'EndOffset': 41,
+#  'PartOfSpeech': {'Tag': 'PRON', 'Score': 0.9978506565093994}},
+# {'TokenId': 12, 'Text': 'parler', 'BeginOffset': 42, 'EndOffset': 48,
+#  'PartOfSpeech': {'Tag': 'VERB', 'Score': 0.9999996423721313}},
+# {'TokenId': 13, 'Text': '.', 'BeginOffset': 48, 'EndOffset': 49,
+#  'PartOfSpeech': {'Tag': 'PUNCT', 'Score': 0.999948263168335}}]
 
 TestPhraseWithMultVerbs = [{'TokenId': 1,
                             'Text': 'Je',
@@ -115,141 +157,198 @@ TestPhrase = [
      }
 ]
 
-
-class ComprehendDetect:
-    """Encapsulates Comprehend detection functions."""
-
-    def __init__(self, comprehend_client):
-        """
-        :param comprehend_client: A Boto3 Comprehend client.
-        """
-        self.comprehend_client = comprehend_client
-
-    def detect_syntax(self, text, language_code):
-        """
-        Detects syntactical elements of a document. Syntax tokens are portions of
-        text along with their use as parts of speech, such as nouns, verbs, and
-        interjections.
-
-        :param text: The document to inspect.
-        :param language_code: The language of the document.
-        :return: The list of syntax tokens along with their confidence scores.
-        """
-        try:
-            response = self.comprehend_client.detect_syntax(
-                Text=text, LanguageCode=language_code)
-            tokens = response['SyntaxTokens']
-            logger.info("Detected %s syntax tokens.", len(tokens))
-        except ClientError:
-            logger.exception("Couldn't detect syntax.")
-            raise
-        else:
-            return tokens
+interrogativePronouns = ['comment', 'quand', 'qui', 'où', 'd\'où', 'combien',
+                         'quel', 'quelle', 'pourquoi', 'quels', 'quelles']
 
 
 def usage_demo():
     print('-' * 88)
-    print("Welcome to the Amazon Comprehend detection demo!")
-    print('-' * 88)
+    # print("Welcome to the Amazon Comprehend detection demo!")
 
-    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    # logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
     comp_detect = ComprehendDetect(boto3.client('comprehend'))
-    with open('detect_sample.txt') as sample_file:
-        sample_text = sample_file.read()
-
-    print("Detecting syntax elements.")
-    # analyze text to find newlines and add question marks
-    # change peux to puis
-    if sample_text.find('peux'):
-        syntax_tokens = comp_detect.detect_syntax(sample_text.replace('peux', 'puis'), 'fr')
+    try:
+        value = input("Mettez votre phrase ici:")
+        print('-' * 88)
+    except SyntaxError:
+        value = ""
+    if not value.lower() == "none":
+        sample_text = value
     else:
-        syntax_tokens = comp_detect.detect_syntax(sample_text, 'fr')
-    # if sample_text.find(','):
+        with open('detect_sample.txt') as sample_file:
+            sample_text = sample_file.read()
+            sample_text = sample_text.lower()
 
-    # print(syntax_tokens)
-    # Syntax_tokens is a dict - the part of speech is a dict within that
+    print("Détectant des éléments de syntaxe.")
+    # analyze text to find newlines and add question marks
+    if not sample_text.find('je peux') == -1:
+        sample_text = sample_text.replace('peux', 'puis')
+    if not sample_text.find('qu\'est-ce que') == -1:
+        sample_text = sample_text.replace('qu\'est-ce que', 'que')
+    elif not sample_text.find('qu\'est-ce qu\'') == -1:
+        sample_text = sample_text.replace('qu\'est-ce qu\'', 'que ')
+    if not sample_text.find('est-ce que') == -1:
+        sample_text = sample_text.replace('est-ce que', '')
+    elif not sample_text.find('est-ce qu\'') == -1:
+        sample_text = sample_text.replace('est-ce qu\'', ' ')
+    syntax_tokens = comp_detect.detect_syntax(sample_text, 'fr')
+    disclaimer = ''
+    try:
+        for i in syntax_tokens:
+            if i['PartOfSpeech']['Score'] < .9:
+                disclaimer = '\n' + color.BOLD + color.YELLOW + "*AVERTISSEMENT*" + color.END + '\n' \
+                             + 'Vu que l\'intervalle de confiance du mot ' + color.BOLD + \
+                             color.YELLOW + i['Text'].upper() + \
+                             color.END + ' est moins de 90%, la précision de ce changement de ' \
+                                         'phrase ne peut être garantie.' + '\n'
 
-    # Use TestPhrase instead of calling the API - this will save some data
-    # Make a list of words with Partof speech
 
-    wordWithPart = []
-    for index, value in enumerate(syntax_tokens):  # Replace with syntax_tokens/TestPhrase when testing
-        wordWithPart.append((value['Text'], value['PartOfSpeech']['Tag']))
+        # Use TestPhrase instead of calling the API - this will save some data
+        # Make a list of words with Partof speech
 
-    # pprint(TestPhrase)
-    # print(wordWithPart[0][0], wordWithPart[0][1])
+        wordWithPart = []
+        for index, value in enumerate(syntax_tokens):  # Replace with syntax_tokens/TestPhrase when testing
+            wordWithPart.append((value['Text'], value['PartOfSpeech']['Tag']))
 
-    # Deal with case. Capitalize first letter - leave proper nouns - lower everything else
-    # If there isn't punctuation after each phrase
-    verbIndex = []
-    pronounIndex = []
-    foundVerb = False
-    foundPron = False
-    for index, each in enumerate(wordWithPart):
-        if not foundVerb and each[1] == 'VERB' or each[1] == 'AUX':  # Distinguish between AUX and VERB
-            # Need to have multiple cases for AUX and for regular VERB -
-            # determine which verbs are AUX - être, avoir, devoir
-            foundVerb = True
-            verbIndex.append(index)
-            # if each[0].upper() == 'PEUX':  # Deal with this later
-            #     each[0].update('Puis')
-            #
-            #     y = list(each)
-            #     y[0] = "Puis"
-            #     each = tuple(y)
+        interrogPron = ''
+        for i, value in enumerate(wordWithPart):
+            if value[0].lower() in interrogativePronouns:
+                wordWithPart.remove(value)
+                interrogPron = value[0] + ' '
+                break
 
-        elif not foundPron and each[1] == 'PRON':
-            foundPron = True
-            pronounIndex.append(index)
-        elif not each[0] == ',' and each[1] == 'PUNCT':
-            foundVerb = False
-            foundPron = False
-    print(wordWithPart)
+        # If there isn't punctuation after each phrase
+        verbIndex = []
+        pronounIndex = []
+        foundVerb = False
+        foundPron = False
+        for index, each in enumerate(wordWithPart):
+            if foundVerb and foundPron:
+                break
+            if not foundVerb and each[1] == 'VERB' or each[1] == 'AUX':  # Distinguish between AUX and VERB
+                foundVerb = True
+                verbIndex.append(index)
 
-    # this only works if there are indices for both lists,
-    # i.e. doesn't work with proper nouns
-    for index, verb in enumerate(verbIndex):
-        wordWithPart[verbIndex[index]], wordWithPart[pronounIndex[index]] = wordWithPart[pronounIndex[index]], \
-                                                                            wordWithPart[verbIndex[index]]
-    stringBuilder = ''
-    # print(wordWithPart)
-    firstWord = True
-    for index, each in enumerate(wordWithPart):
-        if not each[0] == '?' and each[1] == 'PUNCT':
-            stringBuilder = stringBuilder[:-1]
-            if not each[0] == ',':
-                stringBuilder += ' ?'
+            elif not foundPron and each[1] == 'PRON':
+                foundPron = True
+                pronounIndex.append(index)
+            elif not each[0] == ',' and each[1] == 'PUNCT':
+                foundVerb = False
+                foundPron = False
+        # print(wordWithPart)
+
+        # have user indicate gender pronoun to account for this
+        foundPronAfter = False
+        addedPronoun = ""
+        while not foundPron:
+            gender = input("Indiquez le sexe/genre du sujet. Mettez masc ou fém").lower()
+            if gender == "masc":
+                addedPronoun = "il"
+                foundPron = True
+                foundPronAfter = True
+            elif gender == "fém":
+                addedPronoun = "elle"
+                foundPron = True
+                foundPronAfter = True
+        addComma = ''
+        if foundPronAfter:
+            tempTuple = (addedPronoun, 'PRON')
+            for index, word in enumerate(wordWithPart):
+                if word[1] == 'VERB' or word[1] == 'AUX':
+                    wordWithPart.insert(index, tempTuple)
+                    # wordWithPart.insert(index, (',', 'PUNCT'))
+                    addComma = ', '
+                    pronounIndex.append(index)
+                    verbIndex.clear()
+                    verbIndex.append(index + 1)
+                    break
+
+        # Figure out how to insert the pronoun if there is none initially
+        # now figure out just a subject like, "mon père"
+        for index, verb in enumerate(verbIndex):
+            wordWithPart[verbIndex[index]], wordWithPart[pronounIndex[index]] = wordWithPart[pronounIndex[index]], \
+                                                                                wordWithPart[verbIndex[index]]
+        stringBuilder = ''
+        stringBuilder += interrogPron  # TODO: Look at this again
+        tempString = ''
+        firstWord = True
+        index = 0
+        while len(wordWithPart) > 0:
+            if wordWithPart[index][1] != 'VERB' and wordWithPart[index][1] != 'AUX':
+                if firstWord:
+                    if wordWithPart[index][0] == ',':
+                        tempString = tempString[:-1]
+                    if wordWithPart[index][1] == 'DET' and wordWithPart[index][0][len(wordWithPart[index][0]) - 1] == '\'':
+                        tempString += wordWithPart[index][0]
+                    else:
+                        tempString += wordWithPart[index][0] + ' '
+
+                    firstWord = False
+                else:
+                    if wordWithPart[index][0] == ',':
+                        tempString = tempString[:-1]
+                    if wordWithPart[index][1] == 'DET' and wordWithPart[index][0][len(wordWithPart[index][0]) - 1] == '\'':
+                        tempString += wordWithPart[index][0]
+                    else:
+                        tempString += wordWithPart[index][0] + ' '
+
+
+                del wordWithPart[index]
+            else:
+                firstWord = True
+                break
+
+        for index, each in enumerate(wordWithPart):
+            if not each[0] == '?' and each[1] == 'PUNCT':
+                stringBuilder = stringBuilder[:-1]
+                if not each[0] == ',':
+                    stringBuilder += ' ?'
+                    stringBuilder += '\n'
+                    firstWord = True
+                else:
+                    stringBuilder += each[0]
+                    stringBuilder += ' '
+            elif each[0] == '?':
+                stringBuilder += each[0]
                 stringBuilder += '\n'
                 firstWord = True
             else:
-                stringBuilder += each[0]
-                stringBuilder += ' '
-        elif each[0] == '?':
-            stringBuilder += each[0]
-            stringBuilder += '\n'
-            firstWord = True
-        else:
-            if firstWord:
-                stringBuilder += each[0][0].upper() + each[0][1:]
-                stringBuilder += '-'
-                if each[0][-1] == 'a' or each[0][-1] == 'e':
-                    stringBuilder += 't-'
-                firstWord = False
-            else:
-                stringBuilder += each[0].lower()
-                stringBuilder += ' '
-        # if index == pronounIndex:
-        #     stringBuilder += '-'
-        # # elif not each[0] == ',' and each[1] == 'PUNCT':
-        # #     stringBuilder += each[0]
-        # # else:
-        # #     stringBuilder += ' '
-    # stringBuilder += "?"
-    print(stringBuilder)
-    print("Thanks for watching!")
-    print('-' * 88)
+                if firstWord:
+                    if not foundPronAfter:
+                        stringBuilder += each[0]
+                    else:
+                        stringBuilder += each[0]
+                    stringBuilder += '-'
+                    if each[0][-1] == 'a' or each[0][-1] == 'e':  # handle interrogative words like comment and quand
+                        stringBuilder += 't-'
+                    firstWord = False
+                else:
+                    stringBuilder += each[0]
+                    stringBuilder += ' '
+        if '?' not in stringBuilder:
+            stringBuilder += '?'
 
+        print(disclaimer)
+        if tempString[len(tempString) - 1] == ' ' and tempString[len(tempString) - 2] != ',':
+            tempString = tempString[:-1]
+            tempString += ', '
+            # tempString += addComma
+        totalString = tempString + stringBuilder
+        total = ''
+        if totalString.find(' ,') != -1:
+            firstHalf = totalString[:totalString.find(' ,')] + ', '
+            secondHalf = totalString[totalString.find(',') + 1:]
+            print(firstHalf)
+            print(secondHalf)
+            total = firstHalf + secondHalf
+        if len(total) > 0:
+            print(total)
+        else:
+            print(tempString + stringBuilder)
+        print('-' * 88)
+    except IndexError or SyntaxError:
+        print(color.RED + "Incapable d'invertir votre phrase. Veuillez essayer une phrase différente.")
 
 if __name__ == '__main__':
     usage_demo()
